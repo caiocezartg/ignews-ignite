a9c1eeab822f8766895127f3b8c0c1fe4cdefcdf
Caio Cezar <caiocezartg@gmail.com>
2022-06-08 16:46:40 -0300

    feat: Created more webhooks events listeners (updated/cancelled subscription)

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -4,14 +4,19 @@ import { stripe } from "../../../services/stripe";
 
 export async function saveSubscription(
   subscriptionId: string,
-  customerId: string
+  customerId: string,
+  createAction = false
 ) {
+  // Pega a referencia do usuario no FaunaDB
   const userRef = await fauna.query(
     q.Select(
+      // Seleciona apenas o campo 'ref' do banco de dados
       "ref",
       q.Get(q.Match(q.Index("user_by_stripe_customer_id"), customerId))
     )
   );
+
+  //Pega as informacoes da assinatura passando o subscriptionId
   const subscription = await stripe.subscriptions.retrieve(subscriptionId);
 
   const subscriptionData = {
@@ -21,5 +26,20 @@ export async function saveSubscription(
     price_id: subscription.items.data[0].price.id,
   };
 
-  await fauna.query(q.Create(q.Collection("subs"), { data: subscriptionData }));
+  // Salva ou atualiza no FaunaDB, na collection 'subscriptions', o objeto subscriptionData
+  if (createAction) {
+    await fauna.query(
+      q.Create(q.Collection("subscriptions"), { data: subscriptionData })
+    );
+  } else {
+    await fauna.query(
+      q.Replace(
+        q.Select(
+          "ref",
+          q.Get(q.Match(q.Index("subscription_by_id"), subscriptionId))
+        ),
+        { data: subscriptionData }
+      )
+    );
+  }
 }
--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -21,7 +21,11 @@ export const config = {
   },
 };
 
-const relevantEvents = new Set(["checkout.session.completed"]);
+const relevantEvents = new Set([
+  "checkout.session.completed",
+  "customer.subscription.updated",
+  "customer.subscription.deleted",
+]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
@@ -45,20 +49,31 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (relevantEvents.has(type)) {
       try {
         switch (type) {
+          // ATUALIZOU OU CANCELOU A ASSINATURA
+          case "customer.subscription.updated":
+          case "customer.subscription.deleted":
+            //Tipagem unica para o evento de Subscription
+            const subscription = event.data.object as Stripe.Subscription;
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            );
+            break;
+          // NOVO ASSINANTE
           case "checkout.session.completed":
             //Tipagem unica para o evento de Checkout Completed
-            const checkoutSession = event.data[
-              "object"
-            ] as Stripe.Checkout.Session;
+            const checkoutSession = event.data
+              .object as Stripe.Checkout.Session;
 
             await saveSubscription(
               checkoutSession.subscription.toString(),
-              checkoutSession.customer.toString()
+              checkoutSession.customer.toString(),
+              true
             );
             break;
 
           default:
-            // throw new Error("Unhandled event.");
             break;
         }
       } catch (error) {
